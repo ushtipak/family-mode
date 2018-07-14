b0ee80d02c795b165068f82a04054000071b2bc3
Milos Knezevic <ushtipak@gmail.com>
2018-07-14 20:08:43 +0200

    Add initial 'mark home SSID' functionality :)

--- app/src/main/java/com/pijupiju/familymode/MainActivity.java
+++ app/src/main/java/com/pijupiju/familymode/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -12,10 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
     Button btnStartService;
+    Button btnMarkHomeSSID;
     Intent myIntent;
     Boolean serviceEnabled = false;
 
@@ -52,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
                 manageService();
             }
         });
+
+        btnMarkHomeSSID = (Button) findViewById(R.id.btnMarkHomeSSID);
+        btnMarkHomeSSID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                markHomeSSID();
+            }
+        });
     }
 
     private void manageService() {
@@ -76,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MyIntentService.class);
         startService(intent);
         registerReceiver(broadcastReceiver, new IntentFilter("android.net.wifi.STATE_CHANGE"));
-        btnStartService.setText(R.string.disable_family_mode);
+        btnStartService.setText(R.string.btn_disable_family_mode);
     }
 
     private void stopService() {
@@ -89,7 +100,33 @@ public class MainActivity extends AppCompatActivity {
             stopService(myIntent);
         }
         myIntent = null;
-        btnStartService.setText(R.string.enable_family_mode);
+        btnStartService.setText(R.string.btn_enable_family_mode);
+    }
+
+    private void markHomeSSID() {
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        Log.d(TAG, "-> " + methodName);
+
+        String currentSSID = getCurrentSSID();
+        Log.d(TAG, "-> currentSSID: " + currentSSID);
+
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences(getString(R.string.shared_prefs_file), 0);
+        String allMarkedSSIDs = preferences.getString(getString(R.string.shared_prefs_key_ssid), "");
+        Log.d(TAG, "-> allMarkedSSIDs: " + allMarkedSSIDs);
+
+        if (allMarkedSSIDs.contains(currentSSID)) {
+            Log.d(TAG, getString(R.string.usr_msg_ssid_already_registered));
+            Toast.makeText(getApplicationContext(), getString(R.string.usr_msg_ssid_already_registered), Toast.LENGTH_LONG).show();
+        } else {
+            if (!allMarkedSSIDs.equals("")) {
+                allMarkedSSIDs += getString(R.string.shared_prefs_key_ssid_separator);
+                Log.d(TAG, "-> allMarkedSSIDs: " + allMarkedSSIDs);
+            }
+            allMarkedSSIDs += currentSSID;
+            Log.d(TAG, "-> allMarkedSSIDs: " + allMarkedSSIDs);
+            getSharedPreferences(getString(R.string.shared_prefs_file), MODE_PRIVATE).edit().putString(getString(R.string.shared_prefs_key_ssid), allMarkedSSIDs).apply();
+        }
     }
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
@@ -98,27 +135,33 @@ public class MainActivity extends AppCompatActivity {
             }.getClass().getEnclosingMethod().getName();
             Log.d(TAG, "-> " + methodName);
 
-            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            assert wifiManager != null;
-            WifiInfo info = wifiManager.getConnectionInfo();
-            if (info != null) {
-                String wifiName = info.getSSID();
-                Log.d(TAG, "wifiName: " + wifiName);
+            String currentSSID = getCurrentSSID();
+            Log.d(TAG, "-> currentSSID: " + currentSSID);
 
-                String targetWifiName = "\"HOMEWIFI\"";
-                if (wifiName.equals(targetWifiName)) {
-                    Log.d(TAG, "connected to home wifi, disabling ringer :)");
-                    disableRinger();
-                } else {
-                    Log.d(TAG, "connected to some other wifi, don't care, enable ringer :)");
-                    enableRinger();
-                }
+            String targetSSIDName = "\"HOMEWIFI\"";
+            if (currentSSID.equals(targetSSIDName)) {
+                Log.d(TAG, getString(R.string.usr_msg_wifi_connected_to_one_marked_home));
+                disableRinger();
             } else {
-                Log.d(TAG, "not connected to any wifi, enable ringer :))");
+                Log.d(TAG, getString(R.string.usr_msg_wifi_connected_to_non_home));
                 enableRinger();
             }
         }
     };
+
+    private String getCurrentSSID() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        assert wifiManager != null;
+        WifiInfo info = wifiManager.getConnectionInfo();
+        if (info != null) {
+            String currentSSID = info.getSSID();
+            Log.d(TAG, "-> currentSSID: " + currentSSID);
+            return currentSSID;
+        } else {
+            Log.d(TAG, );
+            return "";
+        }
+    }
 
     private void disableRinger() {
         String methodName = new Object() {
@@ -128,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         assert audioManager != null;
         audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-        Log.d(TAG, "audioManager.getRingerMode: " + audioManager.getRingerMode());
+        Log.d(TAG, "-> audioManager.getRingerMode: " + audioManager.getRingerMode());
     }
 
     private void enableRinger() {
@@ -139,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
         final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         assert audioManager != null;
         audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-        Log.d(TAG, "audioManager.getRingerMode: " + audioManager.getRingerMode());
+        Log.d(TAG, "-> audioManager.getRingerMode: " + audioManager.getRingerMode());
     }
 
 }
