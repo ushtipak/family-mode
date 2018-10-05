52fa85afba65b11211625245e40e4fc235c35184
Milos Knezevic <ushtipak@gmail.com>
2018-10-05 18:26:35 +0200

    Working around '[type: WIFI[], state: DISCONNECTED/DISCONNECTED' :)

--- app/src/main/java/com/pijupiju/familymode/WifiReceiver.java
+++ app/src/main/java/com/pijupiju/familymode/WifiReceiver.java
@@ -4,9 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import java.util.ArrayList;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
 
 public class WifiReceiver extends BroadcastReceiver {
     private final static String TAG = WifiReceiver.class.getSimpleName();
@@ -33,32 +37,42 @@ public class WifiReceiver extends BroadcastReceiver {
         }.getClass().getEnclosingMethod().getName();
         Log.d(TAG, "-> " + methodName);
 
-        if (automatic) {
-            Intent intent = new Intent(context, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intent);
-        }
-
         SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(context.getString(R.string.shared_prefs_file), 0);
         Boolean serviceEnabled = preferences.getBoolean(context.getString(R.string.shared_prefs_service_enabled), false);
 
         if (serviceEnabled) {
-            String currentSSID = SSIDManager.getCurrentSSID(context);
-            Log.d(TAG, "-> currentSSID: " + currentSSID);
 
-            ArrayList<String> markedSSIDs = SSIDManager.getMarkedSSIDs(context.getApplicationContext());
-            Log.d(TAG, "-> markedSSIDs: " + markedSSIDs);
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
+            assert connectivityManager != null;
+            NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            Log.d("CAN WE GET THIS:", networkInfo.toString());
+            if (networkInfo.isConnected()) {
 
-            if (markedSSIDs != null) {
-                if (markedSSIDs.contains(currentSSID)) {
-                    Log.d(TAG, context.getString(R.string.log_wifi_connected_to_marked));
-                    RingerManager.disableRinger(context);
-                } else {
-                    Log.d(TAG, context.getString(R.string.log_wifi_connected_to_non_marked));
-                    RingerManager.enableRinger(context);
+                if (automatic) {
+                    Intent intent = new Intent(context, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
                 }
+
+                String currentSSID = SSIDManager.getCurrentSSID(context);
+                Log.d(TAG, "-> currentSSID: " + currentSSID);
+
+                ArrayList<String> markedSSIDs = SSIDManager.getMarkedSSIDs(context.getApplicationContext());
+                Log.d(TAG, "-> markedSSIDs: " + markedSSIDs);
+
+                if (markedSSIDs != null) {
+                    if (markedSSIDs.contains(currentSSID)) {
+                        Log.d(TAG, context.getString(R.string.log_wifi_connected_to_marked));
+                        RingerManager.disableRinger(context);
+                    } else {
+                        Log.d(TAG, context.getString(R.string.log_wifi_connected_to_non_marked));
+                        RingerManager.enableRinger(context);
+                    }
+                }
+            } else {
+                Log.d(TAG, context.getString(R.string.log_wifi_not_connected));
+                RingerManager.enableRinger(context);
             }
         }
-
     }
 }
