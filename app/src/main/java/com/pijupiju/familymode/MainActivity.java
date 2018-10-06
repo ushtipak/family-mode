0fc0cd0ffdf9bd940c1b8ea1733e0c586ab9f858
Milos Knezevic <ushtipak@gmail.com>
2018-10-07 01:44:13 +0200

    Add request permission for f-in fine location :D

--- app/src/main/java/com/pijupiju/familymode/MainActivity.java
+++ app/src/main/java/com/pijupiju/familymode/MainActivity.java
@@ -1,11 +1,15 @@
 package com.pijupiju.familymode;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         initViews();
+        verifyPerms();
     }
 
     @Override
@@ -204,7 +209,20 @@ public class MainActivity extends AppCompatActivity {
             tvRingerState.setText(getString(R.string.tv_state_disabled));
             tvRingerMarked.setVisibility(View.VISIBLE);
         }
+    }
 
+    private void verifyPerms() {
+        Integer REQUEST_ACCESS_FINE_LOCATION = 0;
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_ACCESS_FINE_LOCATION);
+
+            Log.d(TAG, String.valueOf(REQUEST_ACCESS_FINE_LOCATION));
+        }
     }
 
     private void manageService() {
