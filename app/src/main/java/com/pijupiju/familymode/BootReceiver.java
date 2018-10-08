a7d5aa5651b7cf1cf830de00665e3304243d21b2
Milos Knezevic <ushtipak@gmail.com>
2018-10-08 15:17:01 +0200

    Add boot receiver, update manifest and gradle build :)

---
+++ app/src/main/java/com/pijupiju/familymode/BootReceiver.java
@@ -0,0 +1,22 @@
+package com.pijupiju.familymode;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("VANCOOOOVER", "Received action: " + intent.getAction());
+        String action = intent.getAction();
+        assert action != null;
+        if (action.equals("android.intent.action.LOCKED_BOOT_COMPLETED")) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+
+}
